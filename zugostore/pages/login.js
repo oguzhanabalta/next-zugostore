860eb9104b0dfede8bb05fde62450fe2bd3932d8
Oğuzhan BALTA <45859029+oguzhanabalta@users.noreply.github.com>
2021-09-04 19:40:38 +0300

    Builded Login API

--- zugostore/pages/login.js
+++ zugostore/pages/login.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Button, List, ListItem, TextField, Typography, Link} from '@material-ui/core';
 import Layout from '../components/Layout';
 import useStyles from '../utils/styles';
-import NextLink from 'next/link'
+import NextLink from 'next/link';
+import axios from 'axios';
+import data from '../utils/data';
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const classes= useStyles(); 
+    const submitHandler= async (e) => {
+        e.preventDefault();
+        try {
+            const {data} = await axios.post('/api/users/login', {email, password});
+            alert('success login')
+        } catch(err) {
+            alert (err.response.data ? err.response.data.message : err.message)
+        }
+        
+    }
     return (
         <Layout title="Login">
-            <form className={classes.form}>
+            <form onSubmit={submitHandler} className={classes.form}>
                 <Typography component="h3" variant="h3" align="center">Login</Typography>
                 <List>
                     <ListItem>
-                        <TextField variant="outlined" fullWidth id="email" label="Email" inputProps={{type:'email'}}></TextField>
+                        <TextField variant="outlined" fullWidth id="email" label="Email" inputProps={{type:'email'}} onChange={e => setEmail(e.target.value)}></TextField>
                     </ListItem>
                     <ListItem>
-                    <TextField variant="outlined" fullWidth id="password" label="Password" inputProps={{type:'password'}}></TextField>
+                    <TextField variant="outlined" fullWidth id="password" label="Password" inputProps={{type:'password'}} onChange={e => setPassword(e.target.value)}></TextField>
                     </ListItem>
                     <ListItem>
                         <Button variant="contained" type="submit" fullWidth color="primary">Login</Button>
                     </ListItem>
                     <ListItem>
-                        Don't have an account? &nbsp; { ' '} <NextLink href="/register" passHref><Link >Register</Link></NextLink>
+                        Don't have an account? &nbsp; {' '} <NextLink href="/register" passHref><Link >Register</Link></NextLink>
                     </ListItem>
                 </List>
             </form>
