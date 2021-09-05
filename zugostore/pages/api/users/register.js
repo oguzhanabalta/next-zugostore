a5317e0f5fd3dfa6804acc7b6308adccbfb2e00d
Oğuzhan BALTA <45859029+oguzhanabalta@users.noreply.github.com>
2021-09-05 19:30:39 +0300

    Created Register Page

---
+++ zugostore/pages/api/users/register.js
@@ -0,0 +1,29 @@
+import nc from "next-connect";
+import db from "../../../utils/db";
+import User from "../../../models/User";
+import bcrypt from "bcryptjs";
+import { signToken } from "../../../utils/auth";
+const handler = nc();
+
+handler.post(async (req, res) => {
+  await db.connect();
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password),
+    isAdmin: false,
+  });
+  const user = await newUser.save();
+  await db.disconnect();
+
+  const token = signToken(user);
+  res.send({
+    token,
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+  });
+});
+
+export default handler;
--- zugostore/pages/register.js
+++ zugostore/pages/register.js
@@ -8,7 +8,7 @@ import { Store } from '../utils/Store';
 import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 
-export default function Login() {
+export default function Register() {
     const router = useRouter();
     const { redirect } = router.query; // login?redirect=/shipping
     const {state, dispatch}= useContext(Store); 
@@ -18,13 +18,19 @@ export default function Login() {
           router.push('/');
         }
       }, []);
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const classes= useStyles(); 
     const submitHandler= async (e) => {
         e.preventDefault();
+        if(password !== confirmPassword){
+            alert("password don't match ");
+            return;
+        }
         try {
-            const {data} = await axios.post('/api/users/login', {email, password});
+            const {data} = await axios.post('/api/users/register', {name, email, password});
             dispatch({type: 'USER_LOGIN', payload: data});
             Cookies.set('userInfo', data);
             router.push(redirect || '/');
@@ -34,21 +40,27 @@ export default function Login() {
         
     }
     return (
-        <Layout title="Login">
+        <Layout title="Register">
             <form onSubmit={submitHandler} className={classes.form}>
                 <Typography component="h3" variant="h3" align="center">Login</Typography>
                 <List>
                     <ListItem>
-                        <TextField variant="outlined" fullWidth id="email" label="Email" inputProps={{type:'email'}} onChange={e => setEmail(e.target.value)}></TextField>
+                        <TextField variant="outlined" fullWidth id="name" label="Name" inputProps={{type:'text'}} onChange={(e) => setName(e.target.value)}></TextField>
                     </ListItem>
                     <ListItem>
-                    <TextField variant="outlined" fullWidth id="password" label="Password" inputProps={{type:'password'}} onChange={e => setPassword(e.target.value)}></TextField>
+                        <TextField variant="outlined" fullWidth id="email" label="Email" inputProps={{type:'email'}} onChange={(e) => setEmail(e.target.value)}></TextField>
                     </ListItem>
                     <ListItem>
-                        <Button variant="contained" type="submit" fullWidth color="primary">Login</Button>
+                    <TextField variant="outlined" fullWidth id="password" label="Password" inputProps={{type:'password'}} onChange={(e) => setPassword(e.target.value)}></TextField>
                     </ListItem>
                     <ListItem>
-                        Don't have an account? &nbsp; {' '} <NextLink href={`/register?redirect=${redirect || '/'}`} passHref><Link >Register</Link></NextLink>
+                    <TextField variant="outlined" fullWidth id="confirmPassword" label="Confirm Password" inputProps={{type:'password'}} onChange={(e) => setConfirmPassword(e.target.value)}></TextField>
+                    </ListItem>
+                    <ListItem>
+                        <Button variant="contained" type="submit" fullWidth color="primary">Register</Button>
+                    </ListItem>
+                    <ListItem>
+                        Already have an account? &nbsp; {' '} <NextLink href={`/login?redirect=${redirect || '/'}`} passHref><Link >Login</Link></NextLink>
                     </ListItem>
                 </List>
             </form>
